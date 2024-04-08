bc26669f82b9582c123654ade58ff73767fec645
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-08 16:01:39 +0500

    added filter object by keys

---
+++ src/utils/filterObjectByKeys/filterObjectByKeys.ts
@@ -0,0 +1,17 @@
+// This function filters an object based on a provided list of keys.
+// It iterates through the object's keys and checks if they exist in the keys array.
+// If a key is present, it adds the corresponding key-value pair to the filteredObject.
+// The returned object is a partial type of the original object, ensuring type safety.
+
+export const filterObjectByKeys = <T extends object>(
+  obj: T,
+  keys: string[]
+): Partial<T> => {
+  const filteredObject: Partial<T> = {};
+  Object.keys(obj).forEach((key) => {
+    if (keys.includes(key)) {
+      filteredObject[key as keyof T] = obj[key as keyof T];
+    }
+  });
+  return filteredObject;
+};
