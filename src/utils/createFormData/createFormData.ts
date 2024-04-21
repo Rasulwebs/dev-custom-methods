bb5080ec3d6e744e22d23a684b93e24208e76cd0
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-21 21:08:05 +0500

    add create form data methods

---
+++ src/utils/createFormData/createFormData.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Creates a FormData object from the given data object.
+*
+* @param data - The data object to convert to FormData.
+* @returns A FormData object containing the data.
+*/
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const createFormData = <T extends Record<string, any>>(
+  data: T
+): FormData => {
+  const formData = new FormData();
+  for (const key of Object.keys(data)) {
+    formData.append(key, String(data[key])); // Type assertion for string values
+  }
+  return formData;
+};
