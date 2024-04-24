3ce6aa85a92e16207b7b2005824aee5126d074c0
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-24 23:27:44 +0500

    added doc for chunk arr method

--- src/utils/chunkArray/chunkArray.ts
+++ src/utils/chunkArray/chunkArray.ts
@@ -1,5 +1,13 @@
 // Partitioning an array:
 
+/**
+ * Partitions an array into smaller arrays of a specified size.
+ *
+ * @param {T[]} array - The array to partition.
+ * @param {number} size - The size of each partition.
+ * @returns {T[][]} An array containing smaller arrays, each of the specified size (except the last one, which may be smaller).
+ */
+
 const chunkArray = <T>(array: T[], size: number): T[][] => {
   return Array.from({ length: Math.ceil(array.length / size) }, (_, index) =>
     array.slice(index * size, index * size + size)
