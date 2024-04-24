69b086a044749d80f24f85e8d54335babd81cc67
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-24 23:26:07 +0500

    added doc for method

--- src/utils/capitalize/capitalize.ts
+++ src/utils/capitalize/capitalize.ts
@@ -1,5 +1,12 @@
 // Capitalizes the first letter of text:
 
+/**
+ * Capitalizes the first letter of a given string.
+ * 
+ * @param {string} str - The string to capitalize.
+ * @returns {string} The capitalized string.
+ */
+
 const capitalize = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
