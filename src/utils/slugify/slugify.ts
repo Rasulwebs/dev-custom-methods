c57e3b4ad5a018652e1160ea0b8e22f2a7efe075
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-27 14:44:25 +0500

    edited licence and added doc slugify, sortbykey methods

--- src/utils/slugify/slugify.ts
+++ src/utils/slugify/slugify.ts
@@ -1,5 +1,12 @@
 // Creates a slug for the URL of the text:
 
+/**
+ * Creates a slug for the URL of the text.
+ *
+ * @param {string} str - The text to create a slug from.
+ * @return {string} The generated slug for the URL.
+ */
+
 const slugify = (str: string): string => {
   return str
     .toLowerCase()
--- src/utils/sortByKey/sortByKey.ts
+++ src/utils/sortByKey/sortByKey.ts
@@ -1,5 +1,13 @@
 // Sort an array by a specified key:
 
+/**
+ * Sorts an array of objects by a specified key in ascending order.
+ *
+ * @param {T[]} array - The array of objects to be sorted.
+ * @param {keyof T} key - The key in the objects to sort by.
+ * @return {T[]} The sorted array of objects.
+ */
+
 const sortByKey = <T>(array: T[], key: keyof T): T[] => {
   return array.slice().sort((a, b) => (a[key] > b[key] ? 1 : -1));
 };
