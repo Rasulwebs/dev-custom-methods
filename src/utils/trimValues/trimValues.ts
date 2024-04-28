1dfb4ec9878aa8448732127d5ab8d93874369e8b
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-28 13:37:48 +0500

    added doc trim and truncate methods

--- src/utils/trimValues/trimValues.ts
+++ src/utils/trimValues/trimValues.ts
@@ -1,5 +1,11 @@
-
 // Return trimmed string type values in the object
+
+/**
+ * Trims string type values in the object and returns a new object with trimmed values.
+ *
+ * @param {T} obj - The object to trim string values from.
+ * @return {T} A new object with trimmed string type values.
+ */
 
 export function trimValues<T extends Record<string, any>>(obj: T): T {
   const trimmedObject: Partial<T> = {};
--- src/utils/truncate/truncate.tsx
+++
@@ -1,7 +0,0 @@
-// Trim text to specified length:
-
-const truncate = (str: string, maxLength: number): string => {
-  return str.length > maxLength ? str.slice(0, maxLength) + "..." : str;
-};
-
-export default truncate;
