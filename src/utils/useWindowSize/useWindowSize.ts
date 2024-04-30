918c3ee8aac4d410d41d302cce3b4c77f3fa9626
Alexwebs <boboqulovrasul80@gmail.com>
2024-04-30 17:21:56 +0500

    added doc useWindowSize method

--- src/utils/useWindowSize/useWindowSize.ts
+++ src/utils/useWindowSize/useWindowSize.ts
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 
 // This custom hook helps you react to window resizing dynamically:
 
+/**
+ * A custom React hook that dynamically tracks window resizing.
+ * @returns An object containing the width and height of the window.
+ */
+
 const useWindowSize = () => {
   const [size, setSize] = useState<{ width: number; height: number }>({
     width: window.innerWidth,
     height: window.innerHeight,
   });
-
   useEffect(() => {
+    /**
+     * Event handler to update the window size on resize.
+     */
     const handleResize = () => {
       setSize({ width: window.innerWidth, height: window.innerHeight });
     };
