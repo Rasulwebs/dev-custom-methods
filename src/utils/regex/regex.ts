76e1917a5c1fa050e8d7abba13431bcadb2765b2
Alexwebs <boboqulovrasul80@gmail.com>
2024-06-18 23:00:53 +0500

    added regexp doc

--- src/utils/regex/regex.ts
+++ src/utils/regex/regex.ts
@@ -5,4 +5,8 @@ export const regexPhoneNumber: RegExp = /^998(\d{9})$/; // for uzb
 export const regexPassword: RegExp =
   /^(?=.*[0-9])(?=.*[!@#$%^&*()\\[\]{}\\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=.*[A-Z])(?=.*).{8,16}$/;
 
+
+  // Regular expression to match file extensions for homework files.
+  // This regular expression matches file extensions for homework files, which are typically image files (jpg, jpeg, png, gif) and PDF files.
+  
 export const regexHomeworkFile: RegExp = /\.(jpg|jpeg|png|gif|pdf)$/i;
